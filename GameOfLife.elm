29323ae5c1cdd4904230ca9886ce49217aa6e278
Jack Franklin <jack@jackfranklin.net>
2015-10-24 18:22:59 +0100

    Refactor tests; add neighboursForCell

--- GameOfLife.elm
+++ GameOfLife.elm
@@ -8,7 +8,10 @@ cellNextToOther cellOne cellTwo =
     (x1, y1) ->
       case cellTwo of
         (x2, y2) ->
-          (abs (x1 - x2) <= 1) && (abs (y1 - y2) <= 1)
+          (abs (x1 - x2) <= 1) &&
+          (abs (y1 - y2) <= 1) &&
+          -- make sure that we're not dealing with the same cell twice
+          (not (x1 == x2 && y1 == y2))
 
 neighboursForCell: Cell -> List Cell -> List Cell
 neighboursForCell cell cells =
--- GameOfLifeSpec.elm
+++ GameOfLifeSpec.elm
@@ -7,36 +7,19 @@ import ElmTest.Runner.Element exposing (runDisplay)
 
 import GameOfLife exposing (cellNextToOther)
 
+import CellNextToOtherSpec
+import NeighboursForCellSpec
+
 
 givenCell = (2, 1)
 
 tests : Test
 tests =
-    suite "#cellNextToOther"
-        [ test "When the cell is to the left"
-            (assert (cellNextToOther givenCell (1, 1))),
-          test "when the cell is to the right"
-            (assert (cellNextToOther givenCell (3, 1))),
-          test "When the cell is not next"
-            (assert (not (cellNextToOther givenCell (4, 1)))),
-          test "When the cell is close on X but far on Y"
-            (assert (not (cellNextToOther givenCell (3, 5)))),
-          test "When the cell is above"
-            (assert (cellNextToOther givenCell (2, 2))),
-          test "When the cell is below"
-            (assert (cellNextToOther givenCell (2, 0))),
-          test "When the cell is diagonally top left"
-            (assert (cellNextToOther givenCell (1, 2))),
-          test "When the cell is diagonally bottom left"
-            (assert (cellNextToOther givenCell (1, 0))),
-          test "When the cell is diagonally top right"
-            (assert (cellNextToOther givenCell (3, 2))),
-          test "when the cell is diagonally bottom right"
-            (assert (cellNextToOther givenCell (3, 0))),
-          test "when the cell is a negative X"
-            (assert (cellNextToOther (0, 0) (-1, 0)))
-
-        ]
+    suite "GameOfLife"
+      [
+        CellNextToOtherSpec.tests,
+        NeighboursForCellSpec.tests
+      ]
 
 
 main = runDisplay tests
